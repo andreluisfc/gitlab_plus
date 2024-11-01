1134939e000563ed80f6a0f06a442ba3bda05518
andre.candido <andre.candido@consisa.com.br>
2024-11-01 13:26:43 -0300

    padronizado pastas para componentes do site.

--- entrypoints/gitlab.content/projects_list/constants.ts
+++ entrypoints/gitlab.content/projects_list/constants.ts
@@ -3,6 +3,7 @@ export namespace constants {
     export const C_CLASS_PROJECT_LIST_ROW = 'project-row'
     export const C_CLASS_PROJECT_DETAILS = 'project-details'
     export const C_CLASS_NAMESPACE_NAME = 'namespace-name'
+    export const C_CLASS_PROJECT_USER_ACCESS = 'user-access-role'
     export const C_CLASS_PROJECT_NAME = 'project-name'
     export const C_CLAS_AVATAR = 'avatar-container'
 }
--- entrypoints/gitlab.content/projects_list/content.ts
+++ entrypoints/gitlab.content/projects_list/content.ts
@@ -3,6 +3,17 @@ import { constants } from './constants';
 import { style } from './style';
 import '@/entrypoints/gitlab.content/projects_list/style.css';
 
+function AddProjectOwnerTag{
+    const projectDetails = document.querySelectorAll(`.${constants.C_CLASS_PROJECT_DETAILS}`);
+    projectDetails.forEach(projectDetail => {
+        const projectOwner = projectDetail.querySelector(`.${constants.C_CLASS_NAMESPACE_NAME}`);
+        const projectOwnerTag = document.createElement('span');
+        projectOwnerTag.classList.add('gl+_project-owner-tag');
+        projectOwnerTag.textContent = 'Owner';
+        projectOwner.appendChild(projectOwnerTag);
+    });
+}
+
 export namespace projectList {
     export function Initialize(){
         functions.injectStyles(style);
